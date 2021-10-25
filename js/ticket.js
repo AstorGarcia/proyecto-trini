186f748fdca77b5a6dc3c1f40a1d1ca7ef27dce7
Astor <astortomasgarcia@gmail.com>
2021-10-25 20:33:21 -0300

    comienzo

---
+++ js/ticket.js
@@ -0,0 +1,9 @@
+function setAsiento(elemento) {
+  document.getElementById('asiento').innerHTML = elemento.id;
+}
+
+function setHorario(elemento) {
+   document.getElementById('horario').innerHTML = elemento.id;
+}
+
+var Asientos= document.getElementByClass('Asientos');
